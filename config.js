c3d60264813e38f2d52e0a681d39d4a04c870db1
baduyne <125996827+baduyne@users.noreply.github.com>
2023-02-22 00:42:45 +0700

    Add files via upload
    cread

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Khánh Linh';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Happy Birthday 22-2';
+const titleCard = 'Happy Birthday 22-2';
+const contentCard = 'Nhân ngày sinh nhật đá thủ "Châu Nguyễn Khánh Linh" chúc đớ bớt chơi đồ.Không nợ môn , có bằng tiếng anh sớm , ra trường đúng thời hạn.Luôn được hưởng sự hạnh phúc.Chúc bạn một sinh nhật tuyệt vời! Bớt báo lại má ơi ! 💘';
+const giftImage = 'https://scontent.xx.fbcdn.net/v/t1.15752-9/331748793_727822882144323_476729116245172664_n.jpg?stp=dst-jpg_s403x403&_nc_cat=102&ccb=1-7&_nc_sid=aee45a&_nc_ohc=us2hV6oyGMkAX8vXZdt&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdQ0JkCNTGKeSUobEmRdl9qn2SMvY2uf2-_suaae-KR4Qg&oe=641C6A3D';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
